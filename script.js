9ee05d79e7c4cf6158149be7f9e982e3943335d9
megadave66 <davidotuathail@gmail.com>
2024-10-21 19:11:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 password = "Sk1b1d10h10"
 
 function pushButton() {
-    let text;
-    text = window.prompt("Enter your text.");
-    document.write("<h1>Your text: " + text + "</h1>");
+    let userinput = document.getElementById('text').value;
+    document.write("<h1>Your text: " + userinput + "</h1>");
 }
